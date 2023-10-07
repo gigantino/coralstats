8ac58e2e5401f850345f04c74f1324d040f69d89
gigantino <git@gigantino.dev>
2023-10-07 04:18:36 +0200

    feat: add homepage fade

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { navigating } from '$app/stores';
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
 	import searchIcon from '$lib/assets/search.svg';
 	import AlphaBanner from '$lib/components/shared/AlphaBanner.svelte';
 	import Footer from '$lib/components/shared/Footer.svelte';
@@ -21,7 +23,15 @@
 
 	const randomPlayers = ['8hi', 'AfkaraLP', 'PreAke', 'LilVenox', 'TrueF_'];
 
-	$: randomUsername = randomPlayers[Math.floor(Math.random() * randomPlayers.length)];
+	let hasLoaded = false;
+	let searchPlaceholder = '';
+	onMount(() => {
+		searchPlaceholder = `Cerca (e.g. ${
+			randomPlayers[Math.floor(Math.random() * randomPlayers.length)]
+		})`;
+		hasLoaded = true;
+	});
+
 	$: isLoading = $navigating ? true : false;
 </script>
 
@@ -37,30 +47,37 @@
 <main class="main-container flex flex-col justify-center">
 	<AlphaBanner />
 	<div class="w-full flex items-center h-full justify-center flex-col gap-4 px-4">
-		<img
-			src="/logo.webp"
-			alt="coralstats logo"
-			class={`rounded-full shadow-lg w-28 ${isLoading && 'animate-spin'}`}
-		/>
-		<form action={`/player/${username.toLowerCase()}`} autocomplete="off" class="w-fit">
-			<label class="relative">
-				<input
-					on:focus={(e) => e.currentTarget.select()}
-					placeholder={`Cerca (e.g. ${randomUsername})`}
-					type="text"
-					bind:this={searchInput}
-					bind:value={username}
-					on:input={handleInput}
-					class="w-full max-w-sm border border-stone-50/10 bg-stone-800 rounded-lg h-9 px-8 focus:outline-none focus:ring ring-stone-50/10"
-				/>
-				<img src={searchIcon} alt="search" class="absolute top-0 left-2" />
-				<div
-					class="top-0 right-2 border-stone-50/10 border rounded-lg bg-stone-900 w-fit text-sm px-1 absolute"
-				>
-					Ctrl K
-				</div>
-			</label>
-		</form>
+		{#if hasLoaded}
+			<img
+				src="/logo.webp"
+				alt="coralstats logo"
+				class={`rounded-full shadow-lg w-28 ${isLoading && 'animate-spin'}`}
+			/>
+			<form
+				transition:fade
+				action={`/player/${username.toLowerCase()}`}
+				autocomplete="off"
+				class="w-fit"
+			>
+				<label class="relative">
+					<input
+						on:focus={(e) => e.currentTarget.select()}
+						placeholder={searchPlaceholder}
+						type="text"
+						bind:this={searchInput}
+						bind:value={username}
+						on:input={handleInput}
+						class="w-full max-w-sm border border-stone-50/10 bg-stone-800 rounded-lg h-9 px-8 focus:outline-none focus:ring ring-stone-50/10"
+					/>
+					<img src={searchIcon} alt="search" class="absolute top-0 left-2" />
+					<div
+						class="top-0 right-2 border-stone-50/10 border rounded-lg bg-stone-900 w-fit text-sm px-1 absolute"
+					>
+						Ctrl K
+					</div>
+				</label>
+			</form>
+		{/if}
 	</div>
 	<Footer />
 </main>
